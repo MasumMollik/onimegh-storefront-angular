d5587053f29b8d48b779b5cf8e669e8fe447e699
wasifbs23 <wmustahid9@gmail.com>
2024-02-09 00:16:25 +0600

    upate

--- src/app/shared/components/address-form/address-form.component.ts
+++ src/app/shared/components/address-form/address-form.component.ts
@@ -1,19 +1,38 @@
-import {ChangeDetectionStrategy, Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    Input,
+    OnChanges,
+    OnDestroy,
+    OnInit,
+    SimpleChanges,
+} from "@angular/core";
+import {
+    UntypedFormBuilder,
+    UntypedFormGroup,
+    Validators,
+} from "@angular/forms";
 
-import { AddressFragment, CountryFragment, OrderAddressFragment } from '../../../common/generated-types';
-import {DataService, District, Upazilla} from "../../../core/providers/data/data.service";
-import {take, takeUntil} from "rxjs/operators";
-import {Subject} from "rxjs";
+import {
+    AddressFragment,
+    CountryFragment,
+    OrderAddressFragment,
+} from "../../../common/generated-types";
+import {
+    DataService,
+    District,
+    Upazilla,
+} from "../../../core/providers/data/data.service";
+import { take, takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Component({
-    selector: 'vsf-address-form',
-    templateUrl: './address-form.component.html',
+    selector: "vsf-address-form",
+    templateUrl: "./address-form.component.html",
     // styleUrls: ['./address-form.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddressFormComponent implements OnInit, OnChanges, OnDestroy {
-
     @Input() availableCountries: CountryFragment[];
     @Input() address: OrderAddressFragment | AddressFragment;
 
@@ -23,63 +42,72 @@ export class AddressFormComponent implements OnInit, OnChanges, OnDestroy {
     filteredUpazilaByDistrict: Upazilla[] = [];
 
     private destroy$: Subject<string> = new Subject<string>();
-    constructor(private formBuilder: UntypedFormBuilder,
-                private dataService: DataService) {
+
+    constructor(
+        private formBuilder: UntypedFormBuilder,
+        private dataService: DataService,
+    ) {
         this.addressForm = this.formBuilder.group({
-            fullName: '',
-            company: '',
-            streetLine1: ['', Validators.required],
-            streetLine2: '',
-            city: ['', Validators.required],
-            province: '',
-            postalCode: ['', Validators.required],
-            countryCode: ['BD', Validators.required],
-            phoneNumber: '',
+            streetLine1: ["", Validators.required],
+            streetLine2: "",
+            city: ["", Validators.required],
+            province: "",
+            postalCode: ["", Validators.required],
+            phoneNumber: "",
         });
     }
 
     ngOnChanges(changes: SimpleChanges) {
-        if ('address' in changes && this.addressForm && this.address) {
-            this.addressForm.patchValue(this.address, { });
+        if ("address" in changes && this.addressForm && this.address) {
+            this.addressForm.patchValue(this.address, {});
         }
-        const country = this.address && this.address.country;
-        if (country && this.availableCountries) {
-            if (country && typeof country !== 'string') {
-                this.addressForm.patchValue({
-                    countryCode: country.code,
-                });
-            } else {
-                const matchingCountry = this.availableCountries.find(c => c.name === country);
-                if (matchingCountry) {
-                    this.addressForm.patchValue({
-                        countryCode: matchingCountry.code,
-                    });
-                }
-            }
-        }
+        //const country = this.address && this.address.country;
+        //if (country && this.availableCountries) {
+        //if (country && typeof country !== 'string') {
+        //this.addressForm.patchValue({
+        //countryCode: country.code,
+        //});
+        // } else {
+        //const matchingCountry = this.availableCountries.find(c => c.name === country);
+        //if (matchingCountry) {
+        // this.addressForm.patchValue({
+        //   countryCode: matchingCountry.code,
+        // });
+        // }
+        //}
+        //}
     }
 
     ngOnInit(): void {
-        this.addressForm.get('city')?.valueChanges
-            .pipe(takeUntil(this.destroy$))
-            .subscribe(districtId => {
-                const selectedDistrict = this.districts.find( district => district.id === districtId);
+        this.addressForm
+            .get("city")
+            ?.valueChanges.pipe(takeUntil(this.destroy$))
+            .subscribe((districtId) => {
+                const selectedDistrict = this.districts.find(
+                    (district) => district.id === districtId,
+                );
                 if (selectedDistrict) {
-                    this.filteredUpazilaByDistrict = this.upazilas.filter(up => up.district_id === districtId).sort((a, b) => a.name.localeCompare(b.name));
+                    this.filteredUpazilaByDistrict = this.upazilas
+                        .filter((up) => up.district_id === districtId)
+                        .sort((a, b) => a.name.localeCompare(b.name));
                 }
             });
-        this.dataService.getDistricts()
+        this.dataService
+            .getDistricts()
             .pipe(take(1))
-            .subscribe(data => {
-                this.districts = data.sort((a, b) => a.name.localeCompare(b.name));
-                console.log('Districts', data);
+            .subscribe((data) => {
+                this.districts = data.sort((a, b) =>
+                    a.name.localeCompare(b.name),
+                );
+                console.log("Districts", data);
             });
 
-        this.dataService.getUpazillas()
+        this.dataService
+            .getUpazillas()
             .pipe(take(1))
-            .subscribe(data => {
+            .subscribe((data) => {
                 this.upazilas = data;
-                console.log('Upazillas', data);
+                console.log("Upazillas", data);
             });
 
         /*this.dataService.getUnions()
@@ -90,8 +118,7 @@ export class AddressFormComponent implements OnInit, OnChanges, OnDestroy {
     }
 
     ngOnDestroy() {
-        this.destroy$.next('');
+        this.destroy$.next("");
         this.destroy$.complete();
     }
-
 }
